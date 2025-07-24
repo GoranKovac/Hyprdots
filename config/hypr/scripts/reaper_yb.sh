085ceff7de02ff69758adfc46083f9c498cef77e
GoranKovac <gorankovacstudio@gmail.com>
2025-07-24 14:37:02 +0200

    cleaner reaper yabridge refocus vst hack

--- config/hypr/scripts/reaper_yb.sh
+++ config/hypr/scripts/reaper_yb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 targetwindow=$(hyprctl activewindow -j | jq '.class' | grep "REAPER")
-activewindow=$(hyprctl activewindow -j | jq -r .address)
-
 #Limit refocus hack only to Reaper windows
 if [[ ! -z $targetwindow ]]; then
+    activewindow=$(hyprctl activewindow -j | jq -r .address)
+    pid=$(pidof reaper)
     #move focus to main reaper
-    hyprctl dispatch focuswindow 'title:.*(REAPER).*'
+    hyprctl dispatch focuswindow pid:"$pid"
     #return focus to plugin
     hyprctl dispatch focuswindow address:"$activewindow"
 fi
