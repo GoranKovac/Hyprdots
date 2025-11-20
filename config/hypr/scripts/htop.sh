0ce25430748fcc2c38251168af88108fde2e5a05
GoranKovac <gorankovacstudio@gmail.com>
2025-11-20 11:04:29 +0100

    Update for foot terminal

--- config/hypr/scripts/htop.sh
+++ config/hypr/scripts/htop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pgrep htop >/dev/null 2>&1 && killall htop || kitty --class=SYS htop
+pgrep htop >/dev/null 2>&1 && killall htop || foot -a SYS htop
--- config/hypr/scripts/wiremix.sh
+++ config/hypr/scripts/wiremix.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pgrep wiremix >/dev/null 2>&1 && killall wiremix || kitty --class=WIREMIX wiremix
+pgrep wiremix >/dev/null 2>&1 && killall wiremix || foot -a WIREMIX wiremix
